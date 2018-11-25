3c4fea2588e8fb4937233c8097b643361e31e603
zankoo <zhuqi1101@gmail.com>
2018-11-26 00:45:08 +0800

    第二次提交

--- src/main/java/curse/HelloWorld.java
+++ src/main/java/curse/HelloWorld.java
@@ -1,7 +1,9 @@
+package curse;
+
 public class HelloWorld {
     //Hello World输出程序
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.print("Hello World");
         //提交了一次作业
     }
 }
---
+++ src/main/java/curse4/Test.java
@@ -0,0 +1,4 @@
+package curse4;
+
+public class Test {
+}
